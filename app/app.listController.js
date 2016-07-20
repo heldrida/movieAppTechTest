1f0cf1b6c3183f0fa3241be89accbabf5f3ff4ef
Helder <heldrida@gmail.com>
2016-07-21 00:36:34 +0100

    created some placeholders to get the total and list some key data

--- app/app.listController.js
+++ app/app.listController.js
@@ -1,3 +1,22 @@
+var _ = require('lodash');
+
 module.exports = function ($scope) {
-	console.log('list controller loaded!');
+
+	$scope.actorList = [{
+		name: 'name1',
+		salary: 5000	
+	}, {
+		name: 'name2',
+		salary: 65000
+	}, {
+		name: 'name3',
+		salary: 16500
+	}];
+
+	$scope.getTotal = function () {
+		return $scope.actorList.reduce(function(total, actor) {
+			return total + actor.salary;
+		}, 0);
+	}
+
 }
