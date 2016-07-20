dd2859189eb5a9192192d55d6e5af561f3a0ac58
Helder <heldrida@gmail.com>
2016-07-21 00:52:34 +0100

    created some movie list placeholder data, just to create the markup basics

--- app/app.listController.js
+++ app/app.listController.js
@@ -2,19 +2,48 @@ var _ = require('lodash');
 
 module.exports = function ($scope) {
 
-	$scope.actorList = [{
-		name: 'name1',
-		salary: 5000	
+	// todo: service to manage the movie list properly
+	$scope.myMovieList = [{
+		title: 'movieA',
+		category: 'categoryA',
+		subcategory: 'subCategoryA',
+		actors: [{
+			name: 'name1',
+			salary: 5000	
+		}, {
+			name: 'name2',
+			salary: 65000
+		}, {
+			name: 'name3',
+			salary: 16500
+		}]
 	}, {
-		name: 'name2',
-		salary: 65000
-	}, {
-		name: 'name3',
-		salary: 16500
+		title: 'movieB',
+		category: 'categoryB',
+		subcategory: 'subCategoryB',
+		actors: [{
+			name: 'name1',
+			salary: 66000
+		}, {
+			name: 'name2',
+			salary: 12500
+		}, {
+			name: 'name3',
+			salary: 3295
+		}, {
+			name: 'name4',
+			salary: 85300
+		}, {
+			name: 'name5',
+			salary: 85300
+		}, {
+			name: 'name6',
+			salary: 85300
+		}]
 	}];
 
-	$scope.getTotal = function () {
-		return $scope.actorList.reduce(function(total, actor) {
+	$scope.getTotal = function (actors) {
+		return actors.reduce(function(total, actor) {
 			return total + actor.salary;
 		}, 0);
 	}
