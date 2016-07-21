f7f87971497b4cd7aef51fefbf39607e44a47f52
Helder <heldrida@gmail.com>
2016-07-21 02:49:52 +0100

    create sort method. created observer for the sort value;

--- dist/bundle.js
+++ dist/bundle.js
@@ -36195,15 +36195,35 @@
 
 	module.exports = function ($scope, myMovieService) {
 
+		$scope.sort_by = 'desc';
+		$scope.myMovieList = myMovieService.get();
+
 		// todo: service to manage the movie list properly
 		myMovieService.sortBy('desc');
 
-		$scope.myMovieList = myMovieService.get();
+		// observer
+		$scope.$watch(function () {
+			return $scope.sort_by;
+		}, function (o, n) {
+			$scope.myMovieList = myMovieService.sortBy(n);
+		});
 
 		$scope.getTotal = function (actors) {
 			return actors.reduce(function(total, actor) {
 				return total + actor.salary;
 			}, 0);
+		}
+
+		$scope.options = [{
+			id: 0,
+			name: 'asc'
+		}, {
+			id: 1,
+			name: 'desc'
+		}];
+
+		$scope.sortBy = function (opt) {
+			$scope.sort_by = opt;
 		}
 
 	}
